ea9f41d3a16ba617ea1d641a422fddd93d51fa94
Jonathan Hefner <jonathan@hefner.pro>
2024-01-09 17:58:40 -0600

    Test specific behavior of javascript:install:webpack
    This adds test coverage for specific behavior of the
    `javascript:install:webpack` command.

--- test/webpack_installer_test.rb
+++ test/webpack_installer_test.rb
@@ -5,6 +5,26 @@ class WebpackInstallerTest < ActiveSupport::TestCase
   include RailsAppHelpers
   include SharedInstallerTests
 
+  test "webpack installer" do
+    with_new_rails_app do
+      out, _err = run_installer
+
+      File.read("Procfile.dev").tap do |procfile|
+        assert_match "js: yarn build --watch", procfile
+      end
+
+      assert_match "STUBBED gem install foreman", out
+
+      File.read("webpack.config.js").tap do |webpack_config|
+        assert_equal File.read("#{__dir__}/../lib/install/webpack/webpack.config.js"), webpack_config
+      end
+
+      assert_match "STUBBED yarn add webpack", out
+      assert_match %r{STUBBED npm (?:set-script build |pkg set scripts.build=)webpack --config webpack.config.js}, out
+      assert_match "STUBBED yarn build", out
+    end
+  end
+
   private
     def run_installer
       stub_bins("gem", "yarn", "npm")
