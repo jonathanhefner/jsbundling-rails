0ec3d28b58120ae73be55dfe33048b4e1241ccc1
Jonathan Hefner <jonathan@hefner.pro>
2024-01-09 17:52:54 -0600

    Test specific behavior of javascript:install:rollup
    This adds test coverage for specific behavior of the
    `javascript:install:rollup` command.

--- test/rollup_installer_test.rb
+++ test/rollup_installer_test.rb
@@ -5,6 +5,26 @@ class RollupInstallerTest < ActiveSupport::TestCase
   include RailsAppHelpers
   include SharedInstallerTests
 
+  test "rollup installer" do
+    with_new_rails_app do
+      out, _err = run_installer
+
+      File.read("Procfile.dev").tap do |procfile|
+        assert_match "js: yarn build --watch", procfile
+      end
+
+      assert_match "STUBBED gem install foreman", out
+
+      File.read("rollup.config.js").tap do |rollup_config|
+        assert_equal File.read("#{__dir__}/../lib/install/rollup/rollup.config.js"), rollup_config
+      end
+
+      assert_match "STUBBED yarn add rollup", out
+      assert_match %r{STUBBED npm (?:set-script build |pkg set scripts.build=)rollup .*rollup.config.js}, out
+      assert_match "STUBBED yarn build", out
+    end
+  end
+
   private
     def run_installer
       stub_bins("gem", "yarn", "npm")
